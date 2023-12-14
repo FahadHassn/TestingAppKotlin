8326b40e28fd9aa556d3673f87875a314a550041
fahadhassan14 <fahadhassan@mobizion.com>
2023-12-14 18:17:22 +0500

    Email and password validation and other work

--- app/src/main/java/com/example/testingappkotlin/RelativeTaskActivity.kt
+++ app/src/main/java/com/example/testingappkotlin/RelativeTaskActivity.kt
@@ -1,11 +1,149 @@
 package com.example.testingappkotlin
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
 
 class RelativeTaskActivity : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var txtEmail: TextView
+    private lateinit var txtPassword: TextView
+    private lateinit var buttonLight: Button
+    private lateinit var buttonBlack: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relative_task)
+
+        edtEmail = findViewById(R.id._edtEmailR)
+        edtPassword = findViewById(R.id._edtPasswordR)
+        txtEmail = findViewById(R.id._txtValidEmailR)
+        txtPassword = findViewById(R.id._txtValidPasswordR)
+        buttonLight = findViewById(R.id._btnSignInRLight)
+        buttonBlack = findViewById(R.id._btnSignInRBlack)
+        val defaultTextColor = edtEmail.currentTextColor
+
+        edtEmail.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (isValidEmail(edtEmail.text.toString().trim())){
+                    txtEmail.text = "Perfect!"
+                    txtEmail.setTextColor(Color.GREEN)
+                    if (isValidPassword(edtPassword.text.toString().trim())){
+                        buttonLight.isGone = true
+                        buttonBlack.isVisible = true
+                    }else{
+                        buttonLight.isVisible = true
+                    }
+                }else{
+                    txtEmail.text = "Invalid email!"
+                    txtEmail.setTextColor(Color.RED)
+                    buttonBlack.isGone = true
+                    buttonLight.isVisible = true
+                }
+
+                if (edtEmail.text.toString().isEmpty()){
+                    txtEmail.text = "Email should be valid"
+                    txtEmail.setTextColor(defaultTextColor)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+        edtPassword.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (isValidPassword(edtPassword.text.toString().trim())){
+                    txtPassword.text = "Perfect!"
+                    txtPassword.setTextColor(Color.GREEN)
+                    if (isValidEmail(edtEmail.text.toString().trim())){
+                        buttonLight.isGone = true
+                        buttonBlack.isVisible = true
+                    }else{
+                        buttonLight.isVisible = true
+                    }
+                }else{
+                    txtPassword.text = "Invalid password!"
+                    txtPassword.setTextColor(Color.RED)
+                    buttonBlack.isGone = true
+                    buttonLight.isVisible = true
+                }
+
+                if (edtPassword.text.toString().isEmpty()){
+                    txtPassword.text = "Password should be valid"
+                    txtPassword.setTextColor(defaultTextColor)
+
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        })
+
+        //hide and show password
+
+
+        buttonBlack.setOnClickListener {
+//            if (edtEmail.text.toString().isNotEmpty()){
+//                if (!isValidEmail(edtEmail.text.toString().trim())) {
+//                    Toast.makeText(this,"Invalid email address",Toast.LENGTH_SHORT).show()
+//                    return@setOnClickListener
+//                }
+//            }else{
+//                Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show()
+//                return@setOnClickListener
+//            }
+//            if (edtPassword.text.toString().isNotEmpty()){
+//                if (!isValidPassword(edtPassword.text.toString().trim())) {
+//                    Toast.makeText(this, "Password mush have minimum 8 letters\nUppercase and Lowercase letters\n Special symbols and Digits also", Toast.LENGTH_SHORT).show()
+//                    return@setOnClickListener
+//                }
+//            }else{
+//                Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show()
+//                return@setOnClickListener
+//            }
+            Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun isValidPassword(password: String?): Boolean {
+        password?.let {
+            val passwordPattern =
+                "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_+=])(?=\\S+$).{8,}$"
+            val passwordMatcher = Regex(passwordPattern)
+
+            return passwordMatcher.find(password) != null
+        } ?: return false
+    }
+
+    private fun isValidEmail(email: String?): Boolean {
+        val emailPattern = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+        val pattern: Pattern = Pattern.compile(emailPattern)
+        val matcher: Matcher = pattern.matcher(email)
+        return matcher.matches()
     }
 }

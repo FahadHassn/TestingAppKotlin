a629381806f50853506beb6cf3ca6b211f1fd5ff
fahadhassan14 <fahadhassan467@gmail.com>
2024-01-10 18:19:17 +0500

    Add Drawer Navigation

--- app/src/main/java/com/example/testingappkotlin/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/testingappkotlin/ui/dashboard/DashboardFragment.kt
@@ -7,15 +7,24 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.testingappkotlin.R
 import com.example.testingappkotlin.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
+    private lateinit var navController: NavController
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,6 +41,11 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        _binding!!.buttonDashboard.setOnClickListener{
+            navController.navigate(R.id.action_navigation_dashboard_to_recyclerViewSectionTaskActivity)
+        }
+
         return root
     }
 

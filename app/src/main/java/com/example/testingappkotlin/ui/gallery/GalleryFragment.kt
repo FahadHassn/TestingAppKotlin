e3351dafb94c52d45ea4716b335385022db02a22
fahadhassan14 <fahadhassan467@gmail.com>
2024-01-13 18:03:25 +0500

    Get Image From gallery with picasso and dexter

--- app/src/main/java/com/example/testingappkotlin/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/testingappkotlin/ui/gallery/GalleryFragment.kt
@@ -1,17 +1,33 @@
 package com.example.testingappkotlin.ui.gallery
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.PermissionRequest
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.testingappkotlin.databinding.FragmentGalleryBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.single.PermissionListener
+import com.squareup.picasso.Picasso
+import java.util.jar.Manifest
+
 
 class GalleryFragment : Fragment() {
 
     private var _binding: FragmentGalleryBinding? = null
+    var pathUri: Uri? = null
+    var IMAGE_PICKER_CODE = 12
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -32,7 +48,57 @@ class GalleryFragment : Fragment() {
         galleryViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        binding.galleyButton.setOnClickListener {
+//            openGallery()
+            showImage()
+        }
+
         return root
+    }
+
+    private fun showImage() {
+
+        Dexter.withContext(context)
+            .withPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            .withListener(object : PermissionListener{
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                    val intent = Intent(Intent.ACTION_PICK)
+                    intent.setType("image/*")
+                    startActivityForResult(
+                        Intent.createChooser(intent, "Choose Image"),
+                        IMAGE_PICKER_CODE
+                    )
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    Toast.makeText(context,"Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: com.karumi.dexter.listener.PermissionRequest?,
+                    p1: PermissionToken?
+                ) {
+                    TODO("Not yet implemented")
+                }
+
+            }).check()
+
+    }
+
+//    private fun openGallery() {
+//        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+//        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+//    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==IMAGE_PICKER_CODE && resultCode==RESULT_OK){
+            if (data?.data != null) {
+                pathUri = data.data;
+                Picasso.get().load(pathUri).into(binding.galleryImageView);
+            }
+        }
     }
 
     override fun onDestroyView() {

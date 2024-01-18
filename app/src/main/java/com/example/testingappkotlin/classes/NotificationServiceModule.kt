3a34bdeef03b1c3924a29d2ba55f8c7e699a344f
fahadhassan14 <fahadhassan467@gmail.com>
2024-01-18 15:30:05 +0500

    Dependency Injection
    Daggae 2
    @Named() Annotation
    Qualifiers

--- app/src/main/java/com/example/testingappkotlin/classes/NotificationServiceModule.kt
+++ app/src/main/java/com/example/testingappkotlin/classes/NotificationServiceModule.kt
@@ -1,12 +1,22 @@
 package com.example.testingappkotlin.classes
 
+import com.example.testingappkotlin.annotations.MessageQualifier
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class NotificationServiceModule {
+
+    @MessageQualifier
     @Provides
     fun getMessageService(): NotificationService{
         return MessageService()
+    }
+
+    @Named("email")
+    @Provides
+    fun getEmailService(emailService: EmailService): NotificationService{
+        return emailService
     }
 }
--- app/src/main/java/com/example/testingappkotlin/classes/UserRegistrationService.kt
+++ app/src/main/java/com/example/testingappkotlin/classes/UserRegistrationService.kt
@@ -1,15 +1,16 @@
 package com.example.testingappkotlin.classes
 
+import com.example.testingappkotlin.annotations.MessageQualifier
 import javax.inject.Inject
+import javax.inject.Named
 
 class UserRegistrationService @Inject constructor(
     private val userRepository: UserRepository,
-    private val notificationService: NotificationService) {
+    @MessageQualifier private val notificationService: NotificationService) {
 
     fun registerUser(email: String, password: String){
         userRepository.saveUser(email,password)
         notificationService.send(email,"fahad@gmail.com","User Registered")
-
     }
 }
 

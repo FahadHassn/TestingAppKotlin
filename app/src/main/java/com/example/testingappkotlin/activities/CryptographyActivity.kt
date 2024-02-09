b315ccb2cc40c032e09554fa8877dc3ed8ee9ee5
fahadhassan14 <fahadhassan467@gmail.com>
2024-02-09 16:07:23 +0500

    Cryptography

--- app/src/main/java/com/example/testingappkotlin/activities/CryptographyActivity.kt
+++ app/src/main/java/com/example/testingappkotlin/activities/CryptographyActivity.kt
@@ -1,12 +1,28 @@
 package com.example.testingappkotlin.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.testingappkotlin.R
+import com.example.testingappkotlin.databinding.ActivityCryptographyBinding
 
 class CryptographyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cryptography)
+        val binding = ActivityCryptographyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+
+            cryptographyEncryptionButton.setOnClickListener {
+                startActivity(Intent(this@CryptographyActivity,EncryptionActivity::class.java))
+            }
+
+            cryptographyEncryptionButton.setOnClickListener {
+                startActivity(Intent(this@CryptographyActivity,DecryptionActivity::class.java))
+            }
+
+        }
+
     }
 }
